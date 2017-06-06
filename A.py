5acaeb34ac3e31de29f4ab0c8d7cb1e2f665b10e
Jakub Pokrywka <jakubpokrywka@gmail.com>
2017-06-06 00:11:55 +0000

    working on colors now

--- A.py
+++ A.py
@@ -1,14 +1,20 @@
 import RPi.GPIO as GPIO
 import time
+import re
+import sys
+
+
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+
+
 
 
 x = [19,13,5,6,22,27,4,17] # lewa strona
 y = [18,23,24,25,12,16,20,21] # prawa strona
 
 sleep_time = 0.001
-
 
 
 
@@ -22,18 +28,15 @@ def empty():
 			GPIO.output(y_0,GPIO.LOW)
 
 
-def light(points):
-	try:
-		while True:
-			for point in points:
-				GPIO.output(point[0],GPIO.LOW)
-				GPIO.output(point[1],GPIO.HIGH)
-				time.sleep(sleep_time)
-				GPIO.output(point[0],GPIO.HIGH)
-				GPIO.output(point[1],GPIO.LOW)
-	except KeyboardInterrupt:
-		print 'ending'
-		empty()
+def light(points, letter_time):
+	a = time.time()
+	while time.time() - a < letter_time:
+		for point in points:
+			GPIO.output(point[0],GPIO.LOW)
+			GPIO.output(point[1],GPIO.HIGH)
+			time.sleep(sleep_time)
+			GPIO.output(point[0],GPIO.HIGH)
+			GPIO.output(point[1],GPIO.LOW)
 
 
 A_points = [
@@ -51,5 +54,96 @@ A_points = [
 	[x[4],y[6]],
 ]
 
+B_points = [
+	[x[0],y[7]],
+	[x[0],y[6]],
+	[x[0],y[5]],
+	[x[0],y[4]],
+	[x[0],y[3]],
+	[x[0],y[2]],
+	[x[1],y[2]],
+	[x[2],y[2]],
+	[x[3],y[6]],
+	[x[3],y[5]],
+	[x[3],y[4]],
+	[x[3],y[3]],
+	[x[1],y[4]],
+	[x[2],y[4]],
+	[x[1],y[7]],
+	[x[2],y[7]],
+]
+
+
+
+
+C_points = [
+	[x[0],y[7]],
+	[x[0],y[6]],
+	[x[0],y[5]],
+	[x[0],y[4]],
+	[x[0],y[3]],
+	[x[0],y[2]],
+	[x[1],y[2]],
+	[x[2],y[2]],
+	[x[3],y[2]],
+	[x[1],y[7]],
+	[x[2],y[7]],
+	[x[3],y[7]],
+]
+
+
+letter_dict = {'A':A_points,
+		'B':B_points,
+		'C':C_points,
+		}
+
+
+
+def is_letters_valid(letters):
+	return re.search('^[abcABC]*$', letters)	
+
+def is_letters_duration_time_valid(d_time):
+	return re.search('^\d$', d_time) 
+
+
+
+
 empty()
-light(A_points)
+letters = 'abca' # USER DEFINED
+letter_time = '1' # USER DEFINED
+
+
+
+
+try:
+	if not is_letters_valid(letters):
+		print 'given string should contain only A, B and C'
+		sys.exit(1)
+	letters = letters.upper()
+except:
+	print 'given string should contain only A, B and C'
+	sys.exit(1)
+	
+try:
+	if not is_letters_duration_time_valid(letter_time):
+		print 'given time should be integer > 0 and < 10'
+		sys.exit(1)
+	letter_time = int(letter_time)
+except:
+	print 'given time should be integer > 0 and < 10'
+	sys.exit(1)
+
+
+
+try:
+	for letter in letters:
+		print letter
+		light(letter_dict[letter], letter_time)
+except KeyboardInterrupt:
+	print
+	print 'bye'
+	empty()
+	sys.exit(1)
+	
+
+print 'bye'
